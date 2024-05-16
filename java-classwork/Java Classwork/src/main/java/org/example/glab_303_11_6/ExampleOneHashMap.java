f4374d520f4a08f7411d4a568b5ea252c099eca2
JESSIKRU\punki <jessica.a.krueger@gmail.com>
2024-05-15 22:40:48 -0500

    2024.05.15 ctd

---
+++ java-classwork/Java Classwork/src/main/java/org/example/glab_303_11_6/ExampleOneHashMap.java
@@ -0,0 +1,23 @@
+package org.example.glab_303_11_6;
+
+import java.util.HashMap;
+
+public class ExampleOneHashMap {
+
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> languages = new HashMap<>();
+        languages.put(1, "Java");
+        languages.put(2, "Python");
+        languages.put(3, "Javascript");
+        languages.put(4, "C Sharp");
+
+        System.out.println("Hashmap: " + languages);
+
+        //remove element associated with key 2
+        String value = languages.remove(2);
+        System.out.println("Removed value: " + value);
+        System.out.println("Updated HashMap: " + languages);
+
+    }
+}
